29f3eb96aca59b1fa540b06a4a13dab570634be3
srikarn <srikarnanduri628@gmail.com>
2018-09-26 15:26:51 +0530

    Added more Jokes.

--- JokeProviderlib/src/main/java/com/example/android/jokeproviderlib/JokeProviderClass.java
+++ JokeProviderlib/src/main/java/com/example/android/jokeproviderlib/JokeProviderClass.java
@@ -1,9 +1,24 @@
 package com.example.android.jokeproviderlib;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class JokeProviderClass {
 
+    List<String> jokeList = new ArrayList<>();
+
+    public void addJokes(){
+        jokeList.add("I'm really good friends with 25 letters of the alphabet....  I don't know Y.");
+        jokeList.add("Could you please call me a taxi?   \n  Alright, you're a taxi." );
+        jokeList.add("What did the pirate say when he turned 80 \n Aye matey");
+    }
+
     public String tellAJoke(){
-        return "This is a Joke";
+        addJokes();
+        Random random = new Random();
+        String randomJoke = jokeList.get(random.nextInt(jokeList.size()));
+        return  randomJoke;
     }
 
 }
