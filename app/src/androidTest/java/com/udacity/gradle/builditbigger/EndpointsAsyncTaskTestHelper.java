5236317722a48b089d138811bd6a2feaa9e7488c
srikarn <srikarnanduri628@gmail.com>
2018-09-28 09:52:24 +0530

    Added Test Cases

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTestHelper.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTestHelper.java
@@ -25,6 +25,7 @@ class EndpointsAsyncTaskTestHelper extends AsyncTask<Context, Void, String> {
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
+                    .setApplicationName("Build It Bigger")
                     .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
