73784b291f623a06f54ac005088175321fe2f916
srikarn <srikarnanduri628@gmail.com>
2018-09-28 11:09:12 +0530

    Added progressbar.

--- jokedisplaylib/src/main/java/com/example/android/jokedisplaylib/JokeDisplayActivity.java
+++ jokedisplaylib/src/main/java/com/example/android/jokedisplaylib/JokeDisplayActivity.java
@@ -2,24 +2,40 @@ package com.example.android.jokedisplaylib;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class JokeDisplayActivity extends AppCompatActivity {
 
 
     TextView JokeTV;
+    ProgressBar jokePB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_display);
         Intent intent = getIntent();
-        String joke = intent.getStringExtra("joke");
+        final String joke = intent.getStringExtra("joke");
         JokeTV = findViewById(R.id.joke_tv);
+        jokePB = findViewById(R.id.joke_pb);
+        jokePB.setVisibility(View.VISIBLE);
         if (joke != null && joke.length() != 0) {
-            JokeTV.setText(joke);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    jokePB.setVisibility(View.GONE);
+                    JokeTV.setText(joke);
+                }
+            }, 3000);
+
+        } else {
+            String errorMsg= "No Joke Available at the momemt";
+            JokeTV.setText(errorMsg);
         }
     }
 }
