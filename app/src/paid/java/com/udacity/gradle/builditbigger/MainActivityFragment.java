a234380f2e751d71cafd3d5eb78d329e92b4e33a
srikarn <srikarnanduri628@gmail.com>
2018-09-28 10:49:39 +0530

    Added paid and free versions.

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,46 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+   @BindView(R.id.instructions_text_view)
+    TextView mTextView;
+
+   @BindView(R.id.joke_btn)
+    Button joke_btn;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, root);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+
+        String placeHolderText = "Get your daily dose of jokes and start your day with a laugh";
+        mTextView.setText(placeHolderText);
+        return root;
+    }
+}
